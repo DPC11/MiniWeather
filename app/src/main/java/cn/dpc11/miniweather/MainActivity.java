a831bfdfc3b66c9616bdf1253ab28ad42fd40707
DPC <dpc11@qq.com>
2016-09-20 21:05:29 +0800

    完成第一章：初始化工程

---
+++ app/src/main/java/cn/dpc11/miniweather/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.dpc11.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by DPC on 16/9/20.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
